960a3909c8c85a4adcd1eacccfe691bf9941b868
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-05-28 17:44:19 +0530

    Update 2_public_subnets.tf

--- 2_public_subnets.tf
+++ 2_public_subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "task_public_subnet_1"{
     vpc_id = "${aws_vpc.task_vpc.id}"
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "190.160.0.0/24"
     availability_zone = "us-west-2a"
 
     tags = {
@@ -10,7 +10,7 @@ resource "aws_subnet" "task_public_subnet_1"{
 
 resource "aws_subnet" "task_public_subnet_2"{
     vpc_id = "${aws_vpc.task_vpc.id}"
-    cidr_block = "10.0.2.0/24"
+    cidr_block = "190.160.10.0/24"
     availability_zone = "us-west-2b"
 
     tags = {
@@ -20,7 +20,7 @@ resource "aws_subnet" "task_public_subnet_2"{
 
 resource "aws_subnet" "task_public_subnet_3"{
     vpc_id = "${aws_vpc.task_vpc.id}"
-    cidr_block = "10.0.3.0/24"
+    cidr_block = "190.160.11.0/24"
     availability_zone = "us-west-2c"
 
     tags = {
