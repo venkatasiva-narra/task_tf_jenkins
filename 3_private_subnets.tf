86d4efe5bcca8d6ad60be750877c594c0ddba28d
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-05-28 17:45:13 +0530

    Update 3_private_subnets.tf

--- 3_private_subnets.tf
+++ 3_private_subnets.tf
@@ -1,7 +1,7 @@
 
 resource "aws_subnet" "task_private_subnet_1"{
     vpc_id = "${aws_vpc.task_vpc.id}"
-    cidr_block = "10.0.101.0/24"
+    cidr_block = "190.160.15.0/24"
     availability_zone = "us-west-2a"
 
     tags = {
@@ -11,7 +11,7 @@ resource "aws_subnet" "task_private_subnet_1"{
 
 resource "aws_subnet" "task_private_subnet_2"{
     vpc_id = "${aws_vpc.task_vpc.id}"
-    cidr_block = "10.0.102.0/24"
+    cidr_block = "190.160.20.0/24"
     availability_zone = "us-west-2b"
 
     tags = {
@@ -21,7 +21,7 @@ resource "aws_subnet" "task_private_subnet_2"{
 
 resource "aws_subnet" "task_private_subnet_3"{
     vpc_id = "${aws_vpc.task_vpc.id}"
-    cidr_block = "10.0.103.0/24"
+    cidr_block = "190.160.22.0/24"
     availability_zone = "us-west-2c"
 
     tags = {
