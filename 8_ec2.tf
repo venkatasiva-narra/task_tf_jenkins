2fe8bdfb5c8bb728e68f094b7ae61005da20c953
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-05-31 06:19:21 +0530

    Create 8_ec2.tf

---
+++ 8_ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "private_instance"{
+  ami_id        = "ami-0cf6f5c8a62fa5da6"
+  tenancy       = "default"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.task_private_subnet_1.id}"
+  tags = {
+    Name = "private_instance"
+  }
+}
+
+resource "aws_instance" "public_instance"{
+  ami_id        = "ami-0cf6f5c8a62fa5da6"
+  tenancy       = "default"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.task_public_subnet_1.id}"
+  tags ={
+    Name = "public_instance"
+  }
+}
